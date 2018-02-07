2be2e66da106ccaf3cb5f41bbbd40c51bab3b153
Unknown <nathoward22@gmail.com>
2018-02-07 09:53:19 -0700

    Added Physics/Health Scripts

---
+++ Assets/Scripts/HealthPoints.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPoints : MonoBehaviour {
+	public int health; 
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{ 
+		health--;
+		if (health <= 0) {
+			Destroy(gameObject);
+		}
+	}
+
+	//If health reaches 0, destroy
+
+}
