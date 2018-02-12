987538c7b5cff169d36179b329f527205a82a804
Unknown <nathoward22@gmail.com>
2018-02-12 09:41:54 -0700

    Added paddle controls

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+	
+	public static MusicManager instance = null;
+
+	void Awake ()//singleton
+	{
+		//If instance is empty, create instance
+		if (instance == null)
+			instance = this;
+		//If instance is not *this* then destroy it
+		else if (instance != this)
+			Destroy (gameObject);
+		
+		DontDestroyOnLoad (gameObject);
+
+	}
+
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+
+
+	void Update () {
+		//get current paddle position
+		Vector3 paddlePosition = new Vector3 (0, gameObject.transform.position.y,0);
+		//get mouse position
+		float mouseX = Input.mousePosition.x/Screen.width*16-8;
+		//convert mouse position to world space
+		//save new position
+		paddlePosition.x=mouseX;
+		//set this paddle object to saved position
+		this.gameObject.transform.position = paddlePosition;
+	}
+}
