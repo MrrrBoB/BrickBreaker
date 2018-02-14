77381ae90a45455d5e2e9a8a16e263cf2cec50f5
Unknown <nathoward22@gmail.com>
2018-02-14 09:53:11 -0700

    Added sound on collision

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour {
-
+	public bool autoPlay;
+	private GameObject ball;
+	private void Start () {
+		ball = FindObjectOfType<Ball> ().gameObject; 
+	}
 
 
 	void Update () {
+		
 		//get current paddle position
 		Vector3 paddlePosition = new Vector3 (0, gameObject.transform.position.y,0);
+		if (autoPlay) {
+			paddlePosition.x = ball.transform.position.x;
+		}
+		else {
 		//get mouse position
 		float mouseX = Input.mousePosition.x/Screen.width*16-8;
 		//convert mouse position to world space
 		//save new position
-		paddlePosition.x=mouseX;
+			paddlePosition.x=mouseX;}
 		//set this paddle object to saved position
 		this.gameObject.transform.position = paddlePosition;
 	}
