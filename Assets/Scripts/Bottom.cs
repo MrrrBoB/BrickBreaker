357bde13b7c21a0903cbb6611828863fb2885c8d
Unknown <nathoward22@gmail.com>
2018-02-16 09:49:07 -0700

    Added Game Over screen and ends level when ball falls

---
+++ Assets/Scripts/Bottom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottom : MonoBehaviour {
+	public GameManager theGameManager;
+	// Use this for initialization
+	void Start () {
+		theGameManager = FindObjectOfType<GameManager> ();	
+	}
+	private void OnTriggerEnter2D() 
+	{
+		print ("Over");
+		theGameManager.LoadLevel ("GameOver");
+	}
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+
+	public static GameManager instance = null;
+
+	void Awake ()//singleton
+	{
+		//If instance is empty, create instance
+		if (instance == null)
+			instance = this;
+		//If instance is not *this* then destroy it
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+
+	}
+
+
+	public void LoadLevel(string Level)
+	{
+		SceneManager.LoadScene (Level);
+	}
+
+
+
+}
